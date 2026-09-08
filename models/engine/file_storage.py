27a7e2a7dd131fc22687a60044b51d5ac3e3c532
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-09-08 23:15:49 +0200

    Add additional classes to reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,7 +28,22 @@ class FileStorage:
     def reload(self):
         """Deserialize the JSON file to __objects, if it exists."""
         from models.base_model import BaseModel
-        classes = {"BaseModel": BaseModel}
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
+        }
         try:
             with open(FileStorage.__file_path, "r") as f:
                 obj_dict = json.load(f)
