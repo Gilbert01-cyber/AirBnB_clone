3caaedf83546678cebc2097f43febe359904de1e
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-09-08 21:12:04 +0200

    Create file_storage.py

---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class."""
+import json
+
+
+class FileStorage:
+    """Serialize instances to a JSON file and deserialize back."""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return the dictionary __objects."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Set in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serialize __objects to the JSON file (__file_path)."""
+        obj_dict = {key: obj.to_dict()
+                    for key, obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """Deserialize the JSON file to __objects, if it exists."""
+        from models.base_model import BaseModel
+        classes = {"BaseModel": BaseModel}
+        try:
+            with open(FileStorage.__file_path, "r") as f:
+                obj_dict = json.load(f)
+            for key, value in obj_dict.items():
+                cls_name = value["__class__"]
+                FileStorage.__objects[key] = classes[cls_name](**value)
+        except FileNotFoundError:
+            pass
